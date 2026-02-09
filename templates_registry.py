dd9657cc483eaa71def6b4ca45bc6175be3aec15
ronis879 <ronis879@gmail.com>
2026-02-09 16:33:03 +0530

    Phase 3A – Template Registry added

---
+++ templates_registry.py
@@ -0,0 +1,30 @@
+TEMPLATE_REGISTRY = {
+    "GST": {
+        "DRC-01": {
+            "template_code": "T5",
+            "section": "73/74",
+            "description": "Reply to GST DRC-01 show cause notice"
+        },
+        "RECOVERY_ABEYANCE": {
+            "template_code": "T30",
+            "section": "79",
+            "description": "Request to keep recovery in abeyance"
+        }
+    },
+
+    "INCOME_TAX": {
+        "143(2)": {
+            "template_code": "IT-T1",
+            "section": "143(2)",
+            "description": "Reply to scrutiny notice"
+        }
+    },
+
+    "UNIVERSAL": {
+        "COVER_NOTE": {
+            "template_code": "T20",
+            "section": "NA",
+            "description": "Universal cover note"
+        }
+    }
+}
